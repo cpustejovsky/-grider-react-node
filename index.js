a28c741d6987a8c500774324c5f67c8894e0b24d
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-12 09:18:16 -0400

    config variable now full mongo url

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(
-  `mongodb+srv://cpustejovsky:${keys.MONGODB_PASSWORD}@cluster0-rwgbh.mongodb.net/test?retryWrites=true&w=majority`,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose.connect(keys.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
@@ -28,7 +28,12 @@ require("./routes/authRoutes")(app);
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Home Page!</h1><div><a href='/auth/google'>Login</a></div><div><a href='/logout'>Logout</a></div><div><a href='/api/current_user'>Check Current User</a></div>");
+  res.send(
+    "<h1>Home Page!</h1><div><a href='/auth/google'>Login</a></div><div><a href='/logout'>Logout</a></div><div><a href='/api/current_user'>Check Current User</a></div>"
+  );
 });
 
-app.listen(PORT, console.log(`listening on port ${PORT} \n ${process.env.NODE_ENV}`));
+app.listen(
+  PORT,
+  console.log(`listening on port ${PORT} \n ${process.env.NODE_ENV}`)
+);
