af9591c7d39067ab0277e283bbdd3b3bc74e32ad
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-13 08:55:18 -0400

    styling header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                Header
-            </div>
-        )
-    }
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="/" className="left brand-logo">
+            Emaily
+          </a>
+          <ul className="right">
+            <li>
+              <a href="/auth/google">Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header
+
+
+export default Header;
