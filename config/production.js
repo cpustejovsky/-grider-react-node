36ba034fdd89879ba29ee7554408cd089b76c0d7
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-13 12:34:29 -0400

    setting up production keys

--- config/production.js
+++ config/production.js
@@ -3,4 +3,6 @@ module.exports = {
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   MONGODB_URL: process.env.MONGODB_URL,
   COOKIE_KEY: process.env.COOKIE_KEY,
+  STRIPE_API_PUBLIC_KEY: process.env.STRIPE_API_PUBLIC_KEY,
+  STRIPE_API_SECRET_KEY: process.env.STRIPE_API_SECRET_KEY,
 };
