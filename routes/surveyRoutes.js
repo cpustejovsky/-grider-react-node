02453a42a78681606d934f1231ee5f6731fb1f1e
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-19 21:03:36 -0400

    setting up webhook route handling

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
 const requireLogin = require("../middleware/requireLogin");
 const requireCredits = require("../middleware/requireCredits");
 const Mailer = require("../services/Mailer");
@@ -7,12 +10,11 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
-    res.send('Thanks for voting!');
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
+    res.send("Thanks for voting!");
   });
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
-    console.log("hit!")
     const survey = new Survey({
       title,
       subject,
@@ -37,5 +39,24 @@ module.exports = (app) => {
     } catch (err) {
       res.status(422).send(err);
     }
+  });
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const events = _.map(req.body, ({ url, email }) => {
+      const pathname = new URL(url).pathname;
+      const p = new Path("/api/surveys/:surveyId/:choice");
+      const match = p.test(pathname);
+      if (match) {
+        return {
+          email,
+          surveyId: match.surveyId,
+          choice: match.choice,
+        };
+      }
+    });
+
+    const compactEvents = _.compact(events);
+    const deDupedEvents = _.uniqBy(compactEvents, "email", "surveyId");
+    console.log(deDupedEvents)
+    res.send({});
   });
 };
